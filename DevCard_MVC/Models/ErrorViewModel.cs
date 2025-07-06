b13136e24b3c50cefc4e6f2cedd317ed1b0b15b8
faridkhaledian <Farid.khaledian.ai@gmail.com>
2025-07-06 13:06:52 +0330

    Delete DevCard_MVC directory

--- DevCard_MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevCard_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
